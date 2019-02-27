a700447a9e1a5d2a07f15fa17969cc776b30068a
Azad Asanali <azadxperias2012@gmail.com>
2019-02-27 07:40:10 +0530

    Upto Repo config demo

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,19 +2,35 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.*;
 
-
+@Entity
+@Table(name = "goals")
 public class Goal {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "GOAL_ID")
+	private Long id;
+
 	@Range(min = 1, max = 120)
+	@Column(name = "MINUTES")
 	private int minutes;
+
+	public Long getId() {
+		return id;
+	}
 
 	public int getMinutes() {
 		return minutes;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
 	}
-	
+
 }
---
+++ src/main/java/com/pluralsight/service/GoalServiceImpl.java
@@ -0,0 +1,14 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Goal;
+import org.springframework.stereotype.Service;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+    @Override
+    public Goal save(Goal goal) {
+        return null;
+    }
+
+}
